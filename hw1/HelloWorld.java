2d8a9ef8488205a91f59600de628042ccb3a3b32
qolvap <125763359+qolvap@users.noreply.github.com>
2024-02-08 14:58:22 +0100

    Update HelloWorld.java

--- hw1/HelloWorld.java
+++ hw1/HelloWorld.java
@@ -3,8 +3,19 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.print("Hello, World!");
     }
-    // HelloWorld public class has one method which is accesable staticly(it doesnt require creating an object)
-    // main is the name of the method, it has command line parametres.
-    // method contains with one command operation System.out.print
-    // System class has the out filed to print 
+    /* HelloWorld public class has one method which is accesable staticly(it doesnt require creating an object)
+    * main is the name of the method, it has command line parametres.
+    * method contains with one command operation System.out.print
+    * System class has the out filed to print 
+    */
 }
+
+/*
+    * to run the app: 
+
+    *cd ~/Desktop
+    *javac HelloWorld.java
+    *java HelloWorld
+
+    *Hello, World!%                                
+ */
