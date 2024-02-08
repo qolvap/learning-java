f3c6f275a2a7a0dde5f8fc99a32a242d3977f4e7
qolvap <125763359+qolvap@users.noreply.github.com>
2024-02-08 14:45:36 +0100

    hw1
    hw1 first simple java app created in text editor

---
+++ hw1/HelloWorld.java
@@ -0,0 +1,10 @@
+// HelloWorld is the name of the class
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Hello, World!");
+    }
+    // HelloWorld public class has one method which is accesable staticly(it doesnt require creating an object)
+    // main is the name of the method, it has command line parametres.
+    // method contains with one command operation System.out.print
+    // System class has the out filed to print 
+}
