85c0dadee709c0c4bae1a4ab9ff7fbea943e98d4
qolvap <125763359+qolvap@users.noreply.github.com>
2024-02-08 16:50:20 +0100

    Update HelloWorld.java

--- hw1/HelloWorld.java
+++ hw1/HelloWorld.java
@@ -23,3 +23,30 @@ public class HelloWorld {
     *12
     *    java 12
  */
+
+// comment
+
+/*
+ * Class and Package Structure:
+ *
+ * Classes: Java utilizes classes to organize code. Classes define objects and their behavior.
+ * Packages: Packages are a way to organize classes into logical groups. They help avoid naming conflicts and facilitate project management.
+ *
+ * Naming Conventions:
+ *
+ * Class Names: Class names should reflect their purpose. They should start with a capital letter, and words should be separated by camel case (e.g., MyClass).
+ * Package Names: Package names should be unique and related to functionality. They are typically written in reverse order of the company's domain (e.g., com.company.project).
+ *
+ * Creating the Main Method:
+ *
+ * Main Method: Every Java program has an entry point, which is defined by the main method. This method is called when the program is executed.
+ *
+ * Outputting Text:
+ *
+ * Using System.out.println(): This method outputs text to the console. It allows you to display information to the user during program execution.
+ *
+ * Using Tabbing and Special Characters:
+ *
+ * Tabbing: Tabbing is used to align code and improve readability. It can be used for program structuring.
+ * Special Characters: Java supports various special characters, such as \n (newline) and \t (tab), which are used for formatting output text.
+ */
