8dcefaaa159429783ceadb53982cfd05b9f7dbb2
qolvap <125763359+qolvap@users.noreply.github.com>
2024-02-21 19:21:58 +0100

    hw4/ min & max values of the data types

---
+++ hw4: min & max values of the data types/Main (2).java
@@ -0,0 +1,21 @@
+public class Main {
+  public static void main(String[] args) {
+    // Printing the minimum and maximum values for the byte data type
+    System.out.println("byte: " + Byte.MIN_VALUE + " to " + Byte.MAX_VALUE); // byte: -128 to 127
+
+    // Printing the minimum and maximum values for the short data type
+    System.out.println("short: " + Short.MIN_VALUE + " to " + Short.MAX_VALUE); // short: -32768 to 32767
+
+    // Printing the minimum and maximum values for the int data type
+    System.out.println("int: " + Integer.MIN_VALUE + " to " + Integer.MAX_VALUE); // int: -2147483648 to 2147483647
+
+    // Printing the minimum and maximum values for the long data type
+    System.out.println("long: " + Long.MIN_VALUE + " to " + Long.MAX_VALUE); // long: -9223372036854775808 to 9223372036854775807
+
+    // Printing the minimum and maximum values for the float data type
+    System.out.println("float: " + Float.MIN_VALUE + " to " + Float.MAX_VALUE); // float: 1.4E-45 to 3.4028235E38
+
+    // Printing the minimum and maximum values for the double data type
+    System.out.println("double: " + Double.MIN_VALUE + " to " + Double.MAX_VALUE); // double: 4.9E-324 to 1.7976931348623157E308
+  }
+}
